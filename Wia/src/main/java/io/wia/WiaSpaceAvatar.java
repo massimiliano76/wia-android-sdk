7edcf2deeceb18486079bfba077f1e29bfae2e84
Iain Andrew <hi@iainandrew.com>
2019-05-02 12:19:29 +0100

    Added image property to WiaSpaceAvatar

--- Wia/src/main/java/io/wia/WiaSpaceAvatar.java
+++ Wia/src/main/java/io/wia/WiaSpaceAvatar.java
@@ -2,9 +2,14 @@ package io.wia;
 
 public class WiaSpaceAvatar extends WiaResource {
   String thumbnail;
+  String image;
 
   public String thumbnail() {
     return thumbnail;
+  }
+
+  public String image() {
+    return image;
   }
 
 }
